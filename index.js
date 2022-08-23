fb72cd249ed8d515532d4c4d10e9653a111eda39
Pandora <hadrian.diaz@gmail.com>
2022-08-23 09:13:27 -0400

    made level into a class and created a startGame function

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var vertical = `\u2502`;
 var bottomLeft = `\u2514`;
 var bottomRight = `\u2518`;
 
+var wall = `\u2593\u2593`
+
 var playerCharacter = '\u26b2 ';
 //yeah i knoow this couldve been an array but like, i dont care
 //==============================
@@ -19,14 +21,19 @@ var playerCharacter = '\u26b2 ';
 //    mapHeight = 16;
 //map info
 
+class Level {
+    map = [];
+    mapWidth;//for it to look square it should probably be set 4 higher than the height
+    mapHeight;
 
+    constructor(height = 16, width = 20, theY = 0, theX = 0) {
+        this.mapHeight = height;
+        this.mapWidth = width;
 
-//== map object, ill turn this into a class later so i can have multiple maps ==
+        this.playerY = theY;
+        this. playerX = theX;
+    };
 
-var level = {
-    map: [],
-    mapWidth: 20,//for it to look square it should probably be set 4 higher than the height
-    mapHeight: 16,
     generateMap() {
         console.log("Generating map...");
         for (var placeY = 0; placeY < this.mapHeight; placeY++) {
@@ -39,34 +46,34 @@ var level = {
         
         if (this.map.length === 0 || this.map[0].length === 0) console.log("Map generation failed... :<");
         else console.log("Map generated...");
-    },
+    };
 //== player data and movement ==
-    playerY: 0,
-    playerX: 0,
+    playerY;
+    playerX;
     moveUp() {
         console.clear();
         if (this.playerY != 0) this.playerY--;
         else console.log("I can't move any higher");
         this.printMap();
-    },
+    };
     moveDown() {
         console.clear();
         if (this.playerY < this.mapHeight - 1) this.playerY++;
         else console.log("I can't move any lower");
         this.printMap();
-    },
+    };
     moveLeft() {
         console.clear();
         if (this.playerX != 0) this.playerX--;
         else console.log("I can't go left");
         this.printMap();
-    },
+    };
     moveRight() {
         console.clear();
         if (this.playerX < this.mapWidth - 1) this.playerX++;
         else console.log("I can't go right");
         this.printMap();
-    },
+    };
 //== map printing and maybe modification ==
     printMap() {
         //console.debug("Printing function started...");
@@ -117,36 +124,174 @@ var level = {
             //process.stdout.write(`｜ line:${placeY}\n`);
             process.stdout.write(`\n`);
         }
+    }   
+}
+
+function generateMaze(map) {
+
+}
+
+function startGame(map) {
+    var choice = "";
+    map.printMap();
+
+    while (choice != "quit") {
+        choice = readline.question("What would you like to do?\n>");
+        switch (choice) {
+            case "up":
+                map.moveUp();
+                break;
+            case "down":
+                map.moveDown();
+                break;
+            case "left":
+                map.moveLeft();
+                break;
+            case "right":
+                map.moveRight();
+                break;
+            case "quit":
+                break;
+            default: 
+                choice = "quit";
+                break;
+        }
     }
 }
 
-level.generateMap();
-level.printMap();
+console.log(wall);
 
-var choice = "";
+//var mapOne = new Level(16, 20);
+//mapOne.generateMap();
 
-while (choice != "quit") {
-    choice = readline.question("What would you like to do?\n>");
-    switch (choice) {
-        case "up":
-            level.moveUp();
-            break;
-        case "down":
-            level.moveDown();
-            break;
-        case "left":
-            level.moveLeft();
-            break;
-        case "right":
-            level.moveRight();
-            break;
-        case "quit":
-            break;
-        default: 
-            choice = "quit";
-            break;
-    }
-}
+//startGame(mapOne);
+
+//== map object, ill turn this into a class later so i can have multiple maps ==
+
+//var level = {
+//map: [],
+//    mapWidth: 20,//for it to look square it should probably be set 4 higher than the height
+//    mapHeight: 16,
+//    generateMap() {
+//        console.log("Generating map...");
+//        for (var placeY = 0; placeY < this.mapHeight; placeY++) {
+//            var tempArr = [];
+//            for (var placeX = 0; placeX < this.mapWidth; placeX++) {
+//                tempArr.push(empty);
+//            }
+//            this.map.push(tempArr);
+//        }
+//        
+//        if (this.map.length === 0 || this.map[0].length === 0) console.log("Map generation failed... :<");
+//        else console.log("Map generated...");
+//    },
+////== player data and movement ==
+//    playerY: 0,
+//    playerX: 0,
+//    moveUp() {
+//        console.clear();
+//        if (this.playerY != 0) this.playerY--;
+//        else console.log("I can't move any higher");
+//        this.printMap();
+//    },
+//    moveDown() {
+//        console.clear();
+//        if (this.playerY < this.mapHeight - 1) this.playerY++;
+//        else console.log("I can't move any lower");
+//        this.printMap();
+//    },
+//    moveLeft() {
+//        console.clear();
+//        if (this.playerX != 0) this.playerX--;
+//        else console.log("I can't go left");
+//        this.printMap();
+//    },
+//    moveRight() {
+//        console.clear();
+//        if (this.playerX < this.mapWidth - 1) this.playerX++;
+//        else console.log("I can't go right");
+//        this.printMap();
+//    },
+////== map printing and maybe modification ==
+//    printMap() {
+//        //console.debug("Printing function started...");
+//        var fullWidth = this.mapWidth + 2;
+//        var fullHeight = this.mapHeight + 2;
+//        var formatedMap = []; 
+//
+//        var end = fullWidth - 1;
+//
+//        //adding borders to formatedMap, i hope this works because i have to finish the code to insert the map into the formatedMap so i cant print it yet... yikes
+//
+//        //console.log("Creating map borders...");
+//
+//        for (var placeY = 0; placeY < fullHeight; placeY++) {
+//            formatedMap.push([]);
+//            if (placeY === 0) {
+//                formatedMap[placeY][0] = topLeft;
+//                for (var top = 1; top < fullWidth - 1; top++) formatedMap[placeY][top] = horizontal;
+//                formatedMap[placeY][end] = topRight;
+//            }
+//            else if (placeY === fullHeight - 1) {
+//                formatedMap[placeY][0] = bottomLeft;
+//                for (var bottom = 1; bottom < fullWidth - 1; bottom++) formatedMap[placeY][bottom] = horizontal;
+//                formatedMap[placeY][end] = bottomRight
+//            }
+//            else {
+//                formatedMap[placeY][0] = vertical;
+//
+//                for (var placeX = 1; placeX < end; placeX++) formatedMap[placeY][placeX] = this.map[placeY - 1][placeX - 1];
+//
+//                formatedMap[placeY][end] = vertical;
+//            }
+//        }
+//
+//        //if (formatedMap.length === 0 || formatedMap[0].length === 0) console.debug("Map border creation failed...");
+//        //else console.debug("Map borders created...");
+//
+//        //console.debug("Printing finished map!");
+//
+//        //console.debug(formatedMap);
+//
+//        formatedMap[this.playerY + 1][this.playerX + 1] = playerCharacter;
+//
+//        for (var placeY = 0; placeY < fullHeight; placeY++) {
+//            for (var placeX = 0; placeX < fullWidth; placeX++) {
+//                process.stdout.write(formatedMap[placeY][placeX]);
+//            }
+//            //process.stdout.write(`｜ line:${placeY}\n`);
+//            process.stdout.write(`\n`);
+//        }
+//    }
+//}
+//
+//level.generateMap();
+//level.printMap();
+
+//var choice = "";
+
+//while (choice != "quit") {
+//    choice = readline.question("What would you like to do?\n>");
+//    switch (choice) {
+//        case "up":
+//            level.moveUp();
+//            break;
+//        case "down":
+//            level.moveDown();
+//            break;
+//        case "left":
+//            level.moveLeft();
+//            break;
+//        case "right":
+//            level.moveRight();
+//            break;
+//        case "quit":
+//            break;
+//        default: 
+//            choice = "quit";
+//            break;
+//    }
+//}
 //thank fuck everything works!!!
 
 //for (var placeY = 0; placeY < mapHeight; placeY++) {
